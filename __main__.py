228e9c6a40a8d01ad22f9b1ec94001b3b4a84163
Unlisted27 <archerhpo@gmail.com>
2025-03-31 13:56:59 -0300

    adapted to new basicrpg release

--- __main__.py
+++ __main__.py
@@ -18,7 +18,7 @@ class screens():
  /        \|    |___/    |    \/     /_/    |    \    |___/    |    \    |   
 /_______  /|_______ \____|__  /_______ \____|__  /_______ \_______  /____|   
         \/         \/       \/        \/       \/        \/       \/         ''')
-        ans = basicrpg.menu("OPTIONS",["PLAY","EXIT","SETTINGS"])
+        ans = basicrpg.menu("OPTIONS",["PLAY","EXIT","SETTINGS"],return_tuple=True)[1]
         if ans == "PLAY":
             player = screens.char_creator()
             return player
@@ -26,6 +26,7 @@ class screens():
             exit()
         elif ans == "SETTINGS":
             input("SETTINGS COMING SOON")
+            ans = ""
             screens.home_screen()
     def char_creator():
         player = assets.characters.player
@@ -45,16 +46,16 @@ class screens():
         input("Continue to weapon selection?")
         assets.utils.clear_screen()
         ans = basicrpg.menu("CHOOSE YOUR WEAPON",["Sword","Mase","Battleaxe"])
-        if ans == "Sword": player.equip_weapon(assets.game_entities.items.weapons.sword)
-        if ans == "Mase": player.equip_weapon(assets.game_entities.items.weapons.mase)
-        if ans == "Battleaxe": player.equip_weapon(assets.game_entities.items.weapons.battleaxe)
+        if ans == 1: player.equip_weapon(assets.game_entities.items.weapons.sword)
+        if ans == 2: player.equip_weapon(assets.game_entities.items.weapons.mase)
+        if ans == 3: player.equip_weapon(assets.game_entities.items.weapons.battleaxe)
         input()
         assets.utils.clear_screen()
         player.printinvent()
         return player
     def combattest():
         assets.utils._combat([assets.characters.player,assets.characters.companion,assets.characters.goblin])
-    def test():
+    def test(player):
         random_character = assets.utils.gen_character(assets.game_entities.races,assets.game_entities.professions)
         random_character.printstats()
         assets.game_entities.rooms.dungeon_enterance.execute(player)
